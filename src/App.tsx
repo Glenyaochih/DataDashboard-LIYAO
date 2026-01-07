92b8460d59a1e3b6a5b717e423a30217acd561d2
Glen <house5775@gmail.com>
2026-01-08 00:05:05 +0800

    add loading

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,10 @@
-InvoiceBarChart;
-import InvoiceBarChart from './components/InvoiceBarChart';
-import MemberPieChart from './components/MemberPieChart';
+import IndexPage from './pages/IndexPage';
 
 function App() {
   return (
-    <div>
-      <div className='container'>
-        <div className='row'>
-          <div className='col-12 py-5'>
-            <h1 className=' py-5 text-center'>World Member</h1>
-            <div>
-              <MemberPieChart />
-            </div>
-          </div>
-          <div className='col-12 py-5'>
-            <h1 className=' py-5 text-center'>Daily Invoice Quantity</h1>
-            <InvoiceBarChart />
-          </div>
-        </div>
-      </div>
-    </div>
+    <>
+      <IndexPage />
+    </>
   );
 }
 
